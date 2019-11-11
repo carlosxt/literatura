d3440a26580c4227e7002256b4afc026e4bc6a55
Alejandro <ale.lapa3141@gmail.com>
2019-11-11 20:24:35 -0300

    proyecto 2, ya sirve el remotemysql

--- src/main/java/proyecto2/java/connector/Connector.java
+++ src/main/java/proyecto2/java/connector/Connector.java
@@ -7,10 +7,13 @@ public class Connector {
     
     private static String driver="com.mysql.cj.jdbc.Driver";
     //Localhost
-    private static String url="jdbc:mysql://localhost:3306/literatura?useTimezone=true&serverTimezone=UTC";
-    private static String user="root";
-    private static String pass="tripode18";
+    //private static String url="jdbc:mysql://localhost:3306/literatura?useTimezone=true&serverTimezone=UTC";
+    //private static String user="root";
+    //private static String pass="tripode18";
     // useTimezone=true&serverTimezone=UTC  
+    private static String url="jdbc:mysql://remotemysql.com:3306/pzpmsCDtRr";
+    private static String user="pzpmsCDtRr";
+    private static String pass="wOUIB423M5";
    
     private static Connection conn=null;
     private Connector(){}
